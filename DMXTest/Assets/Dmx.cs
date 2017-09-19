3d41f9111c5542c649a57c3cd60502b77faa1f7f
JimSanders96 <jimsandersgamedev@gmail.com>
2017-09-19 10:06:11 +0200

    Initial commit

---
+++ DMXTest/Assets/Dmx.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Showtec;
+
+public class Dmx : MonoBehaviour
+{
+
+    public float intervalSeconds = 0.25f;
+    public bool active = false;
+
+    private float count;
+
+    private void Awake()
+    {
+        OpenDMX.start();
+        ShowtecLLB8.AllOff();
+    }
+
+    private void Update()
+    {
+        count += Time.deltaTime;
+        if (count >= intervalSeconds && active)
+        {
+            count -= intervalSeconds;
+            OpenDMX.writeData();
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+
+    }
+
+    public void AllBlue()
+    {
+        ShowtecLLB8.AllSingleColor(ShowtecLLB8.RGB.BLUE, 255, 255);
+    }
+
+    public void AllRed()
+    {
+        ShowtecLLB8.AllSingleColor(ShowtecLLB8.RGB.RED, 255, 255);
+    }
+
+    public void AllGreen()
+    {
+        ShowtecLLB8.AllSingleColor(ShowtecLLB8.RGB.GREEN, 255, 255);
+    }
+
+}
